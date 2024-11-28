67f0882db254c8a587fc49ed95233346d93f3749
ShrimFry <seminpark05@gmail.com>
2024-11-28 14:07:57 +0900

    Update Weapon_Threeshot.cs
    it now has new variable that called 'burst number' is defining how many fire this weapon going to do

--- Weapon_Threeshot.cs
+++ Weapon_Threeshot.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Weapon_Threeshot : Weapon_front
 {
     float timer;
-    int shotsFired = 0; // 총 발사된 횟수를 추적
-    public float burstInterval = 0.2f; // 3점사 간격
-    public float burstCooldown = 1.0f; // 3점사 후 쉬는 시간
 
+    protected int burst_number = 3;
+    protected float burstInterval = 0.2f; // 3점사 간격
+    protected float burstCooldown = 1.0f; // 3점사 후 쉬는 시간
+
+    int shotsFired = 0; // 총 발사된 횟수를 추적
     public override void onUpdate()
     {
         if (!GameManager.Instance.isLive)
@@ -17,13 +19,13 @@ public class Weapon_Threeshot : Weapon_front
         timer += Time.deltaTime * GameManager.Instance.player.fire_rate;
 
         // 발사 주기와 쉬는 시간에 따라 발사
-        if (shotsFired < 3 && timer > burstInterval)
+        if (shotsFired < burst_number && timer > burstInterval)
         {
             timer = 0f;
             Fire(); // 발사
             shotsFired++; // 발사 횟수 증가
         }
-        else if (shotsFired >= 3 && timer > burstCooldown)
+        else if (shotsFired >= burst_number && timer > burstCooldown)
         {
             // 3점사 후 쉴 때, 발사 횟수 리셋
             shotsFired = 0;
