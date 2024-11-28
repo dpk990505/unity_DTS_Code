27322566a20a4aaaedf350a82f7ed19f806f6590
ShrimFry <seminpark05@gmail.com>
2024-11-28 13:03:04 +0900

    Update Weapon_boom.cs
    as fire function arguments are changed, weapon boom doesn't need to override on update function

--- Weapon_boom.cs
+++ Weapon_boom.cs
@@ -4,27 +4,19 @@ using UnityEngine;
 
 public class Weapon_boom : Weapon
 {
-    float timer;
-   
 
-    // Update is called once per frame
-    public override void onUpdate()
+    public override void Fire()
     {
-        if (!GameManager.Instance.isLive)
-            return;
+        Vector3 start = player.scanner.nearstTargets.position;
+        Vector3 dir = Vector3.down;//목표위치-나의위치
+        dir = dir.normalized;
 
-        
+        Transform bullet = GameManager.Instance.pool.WeaponGet(prefabId).transform;//프리펩ID목록에서 쏠 오브젝트 지정
+        bullet.position = start;//해당 목표 가리킴
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);//지정된 축을 중심으로 목표를 향해 회전
+        bullet.GetComponent<Bullet>().Init(damage * GameManager.Instance.player.power, count, dir, Life_time);
 
-        timer += Time.deltaTime * GameManager.Instance.player.fire_rate;
-        if (timer > speed)
-        {
-            if (!player.scanner.nearstTargets)//지정되는 목표가 없을시
-                return;
-            timer = 0f;
-            Vector3 targetPos = player.scanner.nearstTargets.position;//가까운 적의 위치
-            Fire(targetPos, Vector3.down);
-        }
-
-        
+        AudioManager.Instance.PlaySfx(AudioManager.Sfx.Range);//소리임
     }
+
 }
