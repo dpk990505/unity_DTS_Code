e96a1fa187bdbb3bf3a2616ba4a5ff72974d1370
ShrimFry <seminpark05@gmail.com>
2024-12-03 19:49:28 +0900

    Update Weapon_Boomerang.cs
    now their transform.rotation is added from their basic rotation

--- Weapon_boom.cs
+++ Weapon_boom.cs
@@ -12,8 +12,15 @@ public class Weapon_boom : Weapon
         dir = dir.normalized;
 
         Transform bullet = GameManager.Instance.pool.WeaponGet(prefabId).transform;//프리펩ID목록에서 쏠 오브젝트 지정
+
+        if (!is_rotation_setted)
+        {
+            base_rotation = bullet.rotation;
+            is_rotation_setted = true;
+        }
+
         bullet.position = start;//해당 목표 가리킴
-        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);//지정된 축을 중심으로 목표를 향해 회전
+        bullet.rotation = base_rotation * Quaternion.FromToRotation(Vector3.up, dir);//지정된 축을 중심으로 목표를 향해 회전
         bullet.GetComponent<Bullet>().Init(damage * GameManager.Instance.player.power, count, dir, Life_time);
 
         AudioManager.Instance.PlaySfx(AudioManager.Sfx.Range);//소리임
